5b5254909a896dd272fcf2983b3e65066057c015
Uday <patnaik.udaykiran@gmail.com>
2021-04-26 00:30:08 +0400

    update

--- CurveToGPX.py
+++ CurveToGPX.py
@@ -1,30 +1,30 @@
 import bpy
 
-class ProcessSpline(bpy.types.operator):
+class ProcessSpline(bpy.types.Operator):
     bl_idname = "spline.get_data"
     bl_label = "Get Spline Data" 
 
     def execute(self, context):
         for ob in bpy.context.selected_objects:
-        # Determining the type of the selected object
-        if ob.type == 'CURVE':
-            # Iterating Spline points data
-            for spline in ob.data.splines:
-                # Determining the length of the program
-                if len(spline.bezier_points) > 0:
-                    # Iterating through tht bezier points
-                    for bezier_point in spline.bezier_points.values():
-                        # Writing the file with beziers
-                        file.write('%s,' % str(counter));
-                        # Determing the position in the world
-                        co = ob.matrix_world @ bezier_point.co;
-                        # Determinig the handle position of point tangents
-                        handle_in = ob.matrix_world @ bezier_point.handle_right;
-                        handle_out = ob.matrix_world @ bezier_point.handle_left;
-                        # writing the Handles data
-                        print(f'%s',co)
+            # Determining the type of the selected object
+            if ob.type == 'CURVE':
+                # Iterating Spline points data
+                for spline in ob.data.splines:
+                    # Determining the length of the program
+                    if len(spline.bezier_points) > 0:
+                        # Iterating through tht bezier points
+                        for bezier_point in spline.bezier_points.values():
+                            # Writing the file with beziers
+                            file.write('%s,' % str(counter));
+                            # Determing the position in the world
+                            co = ob.matrix_world @ bezier_point.co;
+                            # Determinig the handle position of point tangents
+                            handle_in = ob.matrix_world @ bezier_point.handle_right;
+                            handle_out = ob.matrix_world @ bezier_point.handle_left;
+                            # writing the Handles data
+                            print("Hello")
 
-        return {'FINISHED'}
+            return {'FINISHED'}
 
 
 def register():
