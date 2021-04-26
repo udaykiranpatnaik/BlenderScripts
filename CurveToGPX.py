edc514b4720378ee9655489efdfb33ebf75fef7f
Uday <patnaik.udaykiran@gmail.com>
2021-04-26 23:16:17 +0400

    xmlString

--- CurveToGPX.py
+++ CurveToGPX.py
@@ -5,14 +5,10 @@ NormalizeFactor = 100
 # Get Spline Curve Infomation
 def GetSplineCurveData(context):
     selected_objects = bpy.context.selected_objects
-#    for ob in selected_objects:
-#        print(ob.name)
-    # check for selected splines
     result = []
     for ob in selected_objects:
         spline_track = {}
         spline_track["id"] = selected_objects.index(ob)
-        spline_track["name"] = ob.name
         spline_track["data"] =[]
         # Determining the type of the selected object
         if ob.type == 'CURVE':
@@ -22,11 +18,12 @@ def GetSplineCurveData(context):
                 if len(spline.bezier_points) > 0:
                     # Iterating through tht bezier points
                     for bezier_point in spline.bezier_points.values():
-                        spline_points = []
+                        #spline_points = []
                         spline_dict = {}
                         spline_dict["Position"] = {}
                         spline_dict["InTangent"] = {}
                         spline_dict["OutTangent"] = {}
+
                         # Determing the position in the world
                         co = ob.matrix_world @ bezier_point.co;
                         spline_dict["Position"]["x"] = co.y * NormalizeFactor 
@@ -38,13 +35,13 @@ def GetSplineCurveData(context):
                         spline_dict["InTangent"]["x"] = handle_in.y * NormalizeFactor
                         spline_dict["InTangent"]["y"] = handle_in.x * NormalizeFactor
                         spline_dict["InTangent"]["z"] = handle_in.z * NormalizeFactor    
-                        
+                         
                         handle_out = ob.matrix_world @ bezier_point.handle_left;
                         spline_dict["OutTangent"]["x"] = handle_out.y * NormalizeFactor
                         spline_dict["OutTangent"]["y"] = handle_out.x * NormalizeFactor
                         spline_dict["OutTangent"]["z"] = handle_out.z * NormalizeFactor   
-                        spline_points.append(spline_dict)
-                        spline_track["data"].append(spline_points)
+                        #spline_points.append(spline_dict)
+                        spline_track["data"].append(spline_dict)
         result.append(spline_track)
     return result               
 
@@ -53,13 +50,24 @@ def GenerateGPX(spline_data):
     xml_header = '<?xml version="1.0" encoding="UTF-8"?> <gpx version="1.1" creator="VeloViewer with Barometer”xmlns="http://www.topografix.com/GPX/1/0"\ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd">'
     xml_track_start = '<trk>'
     xml_track_end = '</trk>'
-    xml_trackname = '<name>' + + '</name>'
+    xml_trackname = '<name>VeloViewer GPX Test</name>'
     xml_tracksegment_start = '<trkseg>'
     xml_tracksegment_end = '</trkseg>'
-    xml_trackpoint = '<trkpt lat="' + + '" lon="' + + '"><ele>'+ +'</ele><time>2021-04-12T00:00:00Z</time></trkpt>'
+    #xml_trackpoint = '<trkpt lat="' + + '" lon="' + + '"><ele>'+ +'</ele><time>2021-04-12T00:00:00Z</time></trkpt>'
     
-    xml_string = xml_header + xm_track_start
-
+    xml_string = xml_header + xml_track_start + xml_trackname
+    for seg in spline_data:
+        xml_string += xml_tracksegment_start
+        track_segments_data = seg.get('data')
+        for track_points in track_segments_data:
+            latitude = track_points.get('Position')['x']
+            longitude = track_points.get('Position')['y']
+            elevation = track_points.get('Position')['z']
+            xml_string += '<trkpt lat="' +str(latitude) + '" lon="' + str(longitude)+ '"><ele>'+ str(elevation) +'</ele><time>2021-04-12T00:00:00Z</time></trkpt>'
+        xml_string += xml_tracksegment_end
+    xml_string += xml_track_end
+    print(xml_string)
+        
 class SplineOperator(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "spline.operator"
@@ -71,7 +79,7 @@ class SplineOperator(bpy.types.Operator):
 
     def execute(self, context):
         points = GetSplineCurveData(context)
-        print(points)
+        GenerateGPX(points)
         return {'FINISHED'}
 
 
@@ -85,6 +93,3 @@ def unregister():
 
 if __name__ == "__main__":
     register()
-
-    # test call
-    #bpy.ops.object.simple_operator()
