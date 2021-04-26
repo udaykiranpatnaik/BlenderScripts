d67d77f6ca6eae1e847aa257aa474fa9cf516ab2
uday kiran patnaik <uday.kiran@avrioc.com>
2021-04-26 14:43:12 +0400

    code update

--- CurveToGPX.py
+++ CurveToGPX.py
@@ -13,37 +13,38 @@ def GetSplineCurveData(context):
         spline_track = {}
         spline_track["id"] = selected_objects.index(ob)
         spline_track["name"] = ob.name
-#        spline_track["data"] =[]
-#        # Determining the type of the selected object
-#        if ob.type == 'CURVE':
-#            # Iterating Spline points data
-#            for spline in ob.data.splines:
-#                # Determining the length of the program
-#                if len(spline.bezier_points) > 0:
-#                    # Iterating through tht bezier points
-#                    for bezier_point in spline.bezier_points.values():
-#                        spline_points = []
-#                        # Determing the position in the world
-#                        spline_dict = {}
-#                        spline_dict["Position"] = {}
-#                        spline_dict["InTangent"] = {}
-#                        spline_dict["OutTangent"] = {}
-#                        co = ob.matrix_world @ bezier_point.co;
-#                        spline_dict["Position"]["x"] = co.y * NormalizeFactor 
-#                        spline_dict["Position"]["y"] = co.x * NormalizeFactor
-#                        spline_dict["Position"]["z"] = co.z * NormalizeFactor
-#                        # Determinig the handle position of point tangents
-#                        handle_in = ob.matrix_world @ bezier_point.handle_right;
-#                        spline_dict["InTangent"]["x"] = handle_in.y * NormalizeFactor
-#                        spline_dict["InTangent"]["y"] = handle_in.x * NormalizeFactor
-#                        spline_dict["InTangent"]["z"] = handle_in.z * NormalizeFactor    
-#                        
-#                        handle_out = ob.matrix_world @ bezier_point.handle_left;
-#                        spline_dict["OutTangent"]["x"] = handle_out.y * NormalizeFactor
-#                        spline_dict["OutTangent"]["y"] = handle_out.x * NormalizeFactor
-#                        spline_dict["OutTangent"]["z"] = handle_out.z * NormalizeFactor   
-#                        spline_points.append(spline_dict)
-#                        spline_track["data"].append(spline_points)
+        spline_track["data"] =[]
+        # Determining the type of the selected object
+        if ob.type == 'CURVE':
+            # Iterating Spline points data
+            for spline in ob.data.splines:
+                # Determining the length of the program
+                if len(spline.bezier_points) > 0:
+                    # Iterating through tht bezier points
+                    for bezier_point in spline.bezier_points.values():
+                        spline_points = []
+                        spline_dict = {}
+                        spline_dict["Position"] = {}
+                        spline_dict["InTangent"] = {}
+                        spline_dict["OutTangent"] = {}
+                        # Determing the position in the world
+                        co = ob.matrix_world @ bezier_point.co;
+                        spline_dict["Position"]["x"] = co.y * NormalizeFactor 
+                        spline_dict["Position"]["y"] = co.x * NormalizeFactor
+                        spline_dict["Position"]["z"] = co.z * NormalizeFactor
+                       
+                        # Determinig the handle position of point tangents
+                        handle_in = ob.matrix_world @ bezier_point.handle_right;
+                        spline_dict["InTangent"]["x"] = handle_in.y * NormalizeFactor
+                        spline_dict["InTangent"]["y"] = handle_in.x * NormalizeFactor
+                        spline_dict["InTangent"]["z"] = handle_in.z * NormalizeFactor    
+                        
+                        handle_out = ob.matrix_world @ bezier_point.handle_left;
+                        spline_dict["OutTangent"]["x"] = handle_out.y * NormalizeFactor
+                        spline_dict["OutTangent"]["y"] = handle_out.x * NormalizeFactor
+                        spline_dict["OutTangent"]["z"] = handle_out.z * NormalizeFactor   
+                        spline_points.append(spline_dict)
+                        spline_track["data"].append(spline_points)
         result.append(spline_track)
     return result               
 
