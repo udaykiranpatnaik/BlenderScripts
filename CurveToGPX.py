ea29c5edd538be1215a5b17dbd7e3bb6b9543a1e
uday kiran patnaik <uday.kiran@avrioc.com>
2021-04-27 11:14:38 +0400

    code update

--- CurveToGPX.py
+++ CurveToGPX.py
@@ -1,4 +1,5 @@
 import bpy
+import os
 
 NormalizeFactor = 100
 
@@ -47,27 +48,46 @@ def GetSplineCurveData(context):
 
 # Generate GPX file from spline data
 def GenerateGPX(spline_data):
-    xml_header = '<?xml version="1.0" encoding="UTF-8"?> <gpx version="1.1" creator="VeloViewer with Barometer”xmlns="http://www.topografix.com/GPX/1/0"\ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd">'
+    xml_header = '<?xml version="1.0" encoding="UTF-8"?>'\
+    '\n'+'<gpx version="1.1" creator="VeloViewer with Barometer"'\
+    '\n'+'xmlns="http://www.topografix.com/GPX/1/0"'\
+    '\n'+'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'\
+    '\n'+'xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd">'
     xml_track_start = '<trk>'
     xml_track_end = '</trk>'
+    xml_gpx_end = '</gpx>'
     xml_trackname = '<name>VeloViewer GPX Test</name>'
     xml_tracksegment_start = '<trkseg>'
     xml_tracksegment_end = '</trkseg>'
     #xml_trackpoint = '<trkpt lat="' + + '" lon="' + + '"><ele>'+ +'</ele><time>2021-04-12T00:00:00Z</time></trkpt>'
     
-    xml_string = xml_header + xml_track_start + xml_trackname
+    xml_string = xml_header + '\n' + xml_track_start + '\n\t' + xml_trackname + '\n'
     for seg in spline_data:
-        xml_string += xml_tracksegment_start
+        xml_string += '\t\t'+xml_tracksegment_start + '\n'
         track_segments_data = seg.get('data')
         for track_points in track_segments_data:
-            latitude = track_points.get('Position')['x']
-            longitude = track_points.get('Position')['y']
-            elevation = track_points.get('Position')['z']
-            xml_string += '<trkpt lat="' +str(latitude) + '" lon="' + str(longitude)+ '"><ele>'+ str(elevation) +'</ele><time>2021-04-12T00:00:00Z</time></trkpt>'
-        xml_string += xml_tracksegment_end
-    xml_string += xml_track_end
-    print(xml_string)
+            latitude = track_points.get('Position')['x']*(0.0000001)
+            longitude = track_points.get('Position')['y']*(0.0000001)
+            elevation = track_points.get('Position')['z']*(0.01)
+            elevation = round(elevation,2)
+            xml_string += '\t\t\t'+'<trkpt lat="' +str(latitude) + '" lon="' + str(longitude)+ '"><ele>"'+ str(elevation) +'"</ele><time>2021-04-12T00:00:00Z</time></trkpt>'+'\n'
+        xml_string += '\t\t' + xml_tracksegment_end +'\n'
+    xml_string += '\t' + xml_track_end +'\n' + xml_gpx_end
+    return xml_string
+    
         
+def WriteToFile(gpx_data):
+    
+    filepath = "D:\Workspace\Scripts\BlenderScripts\VelowviewerGPXTest.gpx"
+    #Opening File in Memory
+    file = open(filepath, 'w', encoding='utf-8')
+   
+    #Writing file contents
+    file.write(gpx_data);
+    
+    #Writing file contents
+    file.close()
+    
 class SplineOperator(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "spline.operator"
@@ -79,7 +99,8 @@ class SplineOperator(bpy.types.Operator):
 
     def execute(self, context):
         points = GetSplineCurveData(context)
-        GenerateGPX(points)
+        gpx_data = GenerateGPX(points)
+        WriteToFile(gpx_data)
         return {'FINISHED'}
 
 
